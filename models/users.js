f90c7b19ee04ee95bbfc59c1a09ee6ba26066ae2
Dmitriy Terementsev <d.terementsev@yandex.ru>
2023-10-10 02:37:06 +0300

    fix

--- models/users.js
+++ models/users.js
@@ -1,29 +1,22 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Поле "name" должно быть заполнено'],
-      minlength: [2, 'Минимальная длина поля "name" - 2'],
-      maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    },
-    about: {
-      type: String,
-      required: [true, 'Поле "about" должно быть заполнено'],
-      minlength: [2, 'Минимальная длина поля "about" - 2'],
-      maxlength: [30, 'Максимальная длина поля "about" - 30'],
-    },
-    avatar: {
-      type: String,
-      validate: {
-        validator: (v) => validator.isURL(v),
-        message: 'Некорректный URL',
-      },
-      required: true,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
-  { versionKey: false }
-);
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model('user', userSchema);
