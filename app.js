a071a9b483e78a0396b0d22a81a3bff98ec2cb1c
Dmitriy Terementsev <d.terementsev@yandex.ru>
2023-10-09 19:33:03 +0300

    fix card test

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
   req.user = {
     _id: '6521bea8d640d437b59db9b1',
@@ -21,6 +18,11 @@ app.use((req, res, next) => {
 
 app.use('/users', routerUsers);
 app.use('/cards', routerCards);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Not Found' });
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
