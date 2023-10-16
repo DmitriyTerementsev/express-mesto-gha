f6bd02549f74c79e6dc5ab0bf956597f9dbcbd0b
Dmitriy Terementsev <d.terementsev@yandex.ru>
2023-10-17 00:35:12 +0300

    test123

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,18 +2,21 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnathorizedError');
 
 module.exports = (req, res, next) => {
-  let payload;
-  const token = req.cookies.jwt;
-  if (!token) {
-    next(new UnauthorizedError('Необходима авторизация'));
-    return;
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
-    return;
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
+
   req.user = payload;
-  next();
+  return next();
 };
